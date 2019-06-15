92994bc9a81a57d9f5a1e77b7895249ba1e16710
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-06-15 13:41:22 +0300

    React Router (77)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './styles/styles.css';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => (
+const Index = () => (
     <p>This is my Boilerplate</p>
 );
 
+const AddExpensePage = () => (
+    <p>This is my AddExpensePage component</p>
+);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const EditExpensePage = () => (
+    <p>This is my EditExpensePage component</p>
+);
+const HelpPage = () => (
+    <p>This is Help Page</p>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path={'/'} component={Index} exact={true}/>
+            <Route path={'/create'} component={AddExpensePage}/>
+            <Route path={'/edit'} component={EditExpensePage}/>
+            <Route path={'/help'} component={HelpPage}/>
+        </div>
+    </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
