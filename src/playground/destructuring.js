5d796c6fe66231ccfb61393932b4f4586530cbef
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-06-20 13:09:20 +0300

    ES6 object destructuring (85)

---
+++ src/playground/destructuring.js
@@ -0,0 +1,29 @@
+// console.log('destructuring');
+//
+// const person = {
+//     // name: 'Andrew',
+//     age: 26,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 32
+//     }
+// };
+// const {name: firstName = 'Anonymous', age} = person;
+// const {city, temp: temperature} = person.location;
+//
+// console.log(`${firstName} is ${age}`);
+//
+//
+// console.log(`It's ${temperature} in ${city}`);
+
+const book = {
+    title: 'Ego is the enemy',
+    author: 'Andrew Mead',
+    publisher: {
+        name: 'Penguin'
+    }
+};
+
+const {name: publisherName = 'Self published'} = book.publisher;
+
+console.log(publisherName);
--- src/playground/redux.js
+++ src/playground/redux.js
@@ -3,15 +3,59 @@ import {createStore} from "redux";
 
 
 export const Redux = (props) => {
-    const store = createStore((state = {count: 0}) => {
-        return state
+    const store = createStore((state = {count: 0}, action) => {
+        switch (action.type) {
+            case 'INCREMENT':
+                // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+                return {
+                    count: state.count + (action.incrementBy || 1)
+                };
+            case 'DECREMENT':
+                return {
+                    count: state.count - (action.decrementBy || 1)
+                };
+            case 'RESET':
+                return {
+                    count: 0
+                };
+            case 'SET':
+                return {
+                    count: action.count
+                };
+            default:
+                return state
+        }
     });
-    console.log(store.getState());
+
+    const unsubscribe = store.subscribe(() => {
+        console.log(store.getState())
+    });
+
+
+    store.dispatch({
+        type: 'INCREMENT',
+        incrementBy: 15
+    });
+
+    store.dispatch({
+        type: 'RESET'
+    });
+    store.dispatch({
+        type: 'DECREMENT',
+        decrementBy: 10
+    });
+
+    store.dispatch({
+        type: 'SET',
+        count: 101
+    });
+    unsubscribe();
+
     return (
         <div>
             this is div
             {
-               props.name
+                props.name
             }
         </div>
     )
