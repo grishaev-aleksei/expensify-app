96bda63b51aa138fddd3d5a8172c51a524b35e8e
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-06-28 21:17:07 +0300

    Organizing redux (99)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppRouter} from "./routers/AppRouter";
 import './styles/styles.css';
 import * as serviceWorker from './serviceWorker';
 
-import {Redux} from "./playground/redux";
-import {ReduxExpensify} from "./playground/redux-expensify";
+import {AppRouter} from "./routers/AppRouter";
+import configureStore from './store/configureStore'
+import {addExpense} from "./actions/expenses";
+import {setTextFilter} from "./actions/filters";
+import {getVisibleExpenses} from "./selectors/expenses";
 
-ReactDOM.render(<ReduxExpensify/>, document.getElementById('root'));
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'water bill'}));
+store.dispatch(addExpense({description: 'gas bill'}));
+
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+console.log(store.getState());
+
+
+ReactDOM.render(AppRouter, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
